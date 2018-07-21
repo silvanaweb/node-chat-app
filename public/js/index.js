42ff1a14ff8850d49edc1d79c9219bbfdd5453ec
Silvana Donato <silvana.donato@accenture.com>
2018-07-21 16:57:16 +0100

    format timestamp with moment

--- public/js/index.js
+++ public/js/index.js
@@ -20,14 +20,16 @@
     });
 
     function addMessageToList(message) {
+        var time = moment(message.createdAt).format('h:mm a');
         var li = $('<li></li>');
-        li.text(`${message.from}: ${message.text}`);
+        li.text(`${message.from} ${time}: ${message.text}`);
         $('#messages').append(li);
     }
     function addMessageLocationToList(message) {
+        var time = moment(message.createdAt).format('h:mm a');
         var li = $('<li></li>');
         var a = $('<a target="_blank">My current location</a>');
-        li.text(`${message.from}: `);
+        li.text(`${message.from} ${time}: `);
         a.attr('href', message.url);
         li.append(a);
 
@@ -37,13 +39,15 @@
 
     $('#myform').on('submit', function (e) {
         e.preventDefault();
+        var msgBox = $('#message');
         socket.emit(
             'createMessage',
             {
                 from: "Frank",
-                text: $('#message').val()
+                text: msgBox.val()
             },
             function (data) {
+                msgBox.val('');
                 console.log('Messae sent:', data);
             }
         );
@@ -54,13 +58,15 @@
         if (!navigator["geolocation"]) {
             return alert('Geolocation not available by your browser');
         }
-
+        locationButton.attr('disabled', 'disabled').text('Sending location...');
         navigator.geolocation.getCurrentPosition(function (position) {
+            locationButton.removeAttr('disabled').text('Send location');
             socket.emit('createLocationMessage', {
                 latitude: position.coords.latitude,
                 longitude: position.coords.longitude
             });
         }, function () {
+            locationButton.removeAttr('disabled').text('Send location');
             alert('Unable to fetch locaiton');
         });
     })
